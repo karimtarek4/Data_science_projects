828c8ed6f15fd34bbf2a6a3758b18a119f4cb905
mac <mac@macs-MacBook-Pro.local>
2023-08-29 16:35:42 +0300

    added check connection function and integrated at vital parts of code to avoid parsing error

--- Glassdoor Webscrapper/gd_job_listing_scraper.py
+++ Glassdoor Webscrapper/gd_job_listing_scraper.py
@@ -3,13 +3,32 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
+import socket
 import time
+    
 import pandas as pd
+
+# Function to check network connectivity
+def check_network_connection():
+    while True:
+        try:
+            # Try to establish a socket connection to a known website (like Google)
+            socket.create_connection(("www.google.com", 80))
+            print("Network connection is active.")
+            return True
+        except OSError:
+            print("Network connection is not active. Waiting...")
+            time.sleep(30)  # Wait for 10 seconds before checking again
+
 
 
 # Function to click the "Show More" button to expand job details
 def click_show_more(driver):
+    # Verify network connection first 
+    check_network_connection()
+    
     try:
         show_more_less = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".css-t3xrds.e856ufb4")))
     except:
@@ -19,18 +38,24 @@ def click_show_more(driver):
         # Scroll the element into view
         driver.execute_script("arguments[0].scrollIntoView();", show_more_less)
         driver.execute_script("arguments[0].click();", show_more_less)
+        time.sleep(10)
         return True
     else:
         return False
 
 # Function to click the "Next" button for pagination
 def click_next_button(driver):
+    # Verify network connection first 
+    check_network_connection()
+    
+    # Execute code
     try:
-        time.sleep(10)
-        next_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//button[@data-test="pagination-next"]')))
+        next_button = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, '//button[@data-test="pagination-next"]')))
         driver.execute_script("arguments[0].scrollIntoView();", next_button)
         driver.execute_script("arguments[0].click();", next_button)
-        time.sleep(15)
+        time.sleep(10)
+      
+        
     except NoSuchElementException:
         print("Reached the last page or unable to find the 'Next' button")
         return False
@@ -38,6 +63,10 @@ def click_next_button(driver):
 
 # Function to extract job details from a listing
 def extract_job_details(listing, driver):
+    # Verify network connection first 
+    check_network_connection()
+    
+    #Execude code 
     job_details = {}
     
     # Extract job title
@@ -150,13 +179,16 @@ def extract_job_details(listing, driver):
         job_details["Revenue"] = revenue
     
     except NoSuchElementException:
-        print("Company info not found.")
+        print("Company info not fetched******.")
     
     return job_details
 
 
 # Function to initiate the WebDriver with retries
 def initiate_webdriver():
+    # Verify network connection first 
+    check_network_connection()
+    
     max_attempts = 5  # Maximum number of attempts to establish WebDriver
     
     for attempt in range(1, max_attempts + 1):
@@ -167,7 +199,7 @@ def initiate_webdriver():
             return driver
         except WebDriverException:
             print(f"Attempt {attempt}/{max_attempts}: WebDriver initiation failed. Retrying...")
-            time.sleep(5)  # Wait before retrying
+            time.sleep(60)  # Wait before retrying
     
     print(f"Failed to initiate WebDriver after {max_attempts} attempts.")
     return None
@@ -178,10 +210,32 @@ def export_csv(jobs, page_num, job_title):
     csv_filename = f'{job_title}_page_{page_num}.csv'
     job_df.to_csv(csv_filename, index=False, encoding='utf-8-sig')
     print(f"Exported job listings to '{csv_filename}'")
+    
+    
+# Function to print extracted job details in consol (tracing)
+def print_job_details(job_index, page_num, job_details):
+    print(f"Job {job_index} (Page {page_num}):")
+    print("/" * 10)
+    print("Location:", job_details.get("Location", "N/A"))
+    print("Job Title:", job_details.get("Job Title", "N/A"))
+    print("Salary Estimate:", job_details.get("Salary Estimate", "N/A"))
+    print("Employer Name:", job_details.get("Employer Name", "N/A"))
+    print("Job Description:", job_details.get("Job Description", "N/A")[:200])  # Limit to 200 characters
+    print("Rating:", job_details.get("Rating", "N/A"))
+    print("Company Size:", job_details.get("Company Size", "N/A"))
+    print("Founded:", job_details.get("Founded", "N/A"))
+    print("Type:", job_details.get("Type", "N/A"))
+    print("Industry:", job_details.get("Industry", "N/A"))
+    print("Sector:", job_details.get("Sector", "N/A"))
+    print("Revenue:", job_details.get("Revenue", "N/A"))
+    
 
 # Main function to initiate scraping
 # Export_interval= export every 2 pages
 def main(job_title, export_interval=1):
+    # Verify network connection first 
+    check_network_connection()
+    
     print("-"*30)
     print("Fetching for: ", job_title)
     
@@ -205,11 +259,10 @@ def main(job_title, export_interval=1):
     jobs = []
     
     # Click next until reaching start page
-    for _ in range(page_start - 1):
-        print("Next page >>")
-        if not click_next_button(driver):
-            print("Reached the desired starting page.")
-            break
+    # for _ in range(page_start - 1):
+    #     if not click_next_button(driver):
+    #         print("Reached the desired starting page.")
+    #         break
     
     
     for page_num in range(page_start, int(page_end) + 1):
@@ -222,17 +275,16 @@ def main(job_title, export_interval=1):
         # Iterate over job listings
         for listing in job_listings:
             driver.execute_script("arguments[0].click();", listing)
-            time.sleep(10)
+            time.sleep(15)
             click_show_more(driver)
-            time.sleep(10)
             job_details = extract_job_details(listing, driver)
             if job_details not in jobs:
                 job_index = len(jobs) + 1  # Get the current index of the job in the jobs list
-                print(job_details)
-                print(f"Job {job_index} (Page {page_num}):")
-                print("/" * 10)
+                print_job_details(job_index, page_num, job_details)
                 jobs.append(job_details)
-        
+            else: 
+                # If already inserted in the array wait to give website time to load
+                time.sleep(5)
         if not click_next_button(driver):
             break
             
@@ -246,9 +298,12 @@ def main(job_title, export_interval=1):
 ## Main function call
 # job_titles = ['data_scientist', 'data analyst', 'machine learning', 'data engineer', 'business intelligence analyst', 'business intelligence developer']
 
-job_titles = ['data_scientist']
+job_titles = ['data_analyst']
 
 jobs = []
+
+# Verify network connection first 
+check_network_connection()
 
 # Scraping job listings and creating DataFrames
 for title in job_titles:
